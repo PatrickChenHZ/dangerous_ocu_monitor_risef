cad80c61fb1d011d453cf2c22f7bac7230d7f95a
PatrickChenHZ <33338982+PatrickChenHZ@users.noreply.github.com>
2022-01-04 21:22:08 -0500

    Wristband Software V1.1
    Major Version Upgrade v1.0-f to v1.1
    The user profile is no longer stored locally on the wristband, it is now dynamically allocated to the wristband upon boot by the server.
    The server assign profile based on Hardware UUID(unique id) of each wristband. This is a very important step ahead for the entire project as it is no longer required to reprogram the wristband each time user/facility allocation changes, all job can be done on the server.
    Because of that, now the time is also announced by the server to the wristband, after boot up, the RTC in the wristband keep track of the time
    Will sync Server config in next commit

--- WristBand_Software/wristband_v1/src/main.cpp
+++ WristBand_Software/wristband_v1/src/main.cpp
@@ -71,9 +71,22 @@ String page3buffer = "";
 bool slept= false;
 String chour,cminute;
 
-//Static
-String user = "Patr C.";
-char permitted = 's';
+bool validprofile = false;
+
+//Debug Statics
+//String user = "Patr C.";
+//char permitted = 's';
+
+//profile
+String user = "";
+char permitted[50];
+//assumed 50 zones, there is no freaking vector in arduino
+String zoneid[50];
+char allowedzonerating[50];
+bool configured = false;
+
+//Device ID
+const char* hwuuid = "d8a01d5b4636";
 
 //mqtt
 const char* mqtt_server = "192.168.123.16";
@@ -169,7 +182,7 @@ void infoscreen(){
   tft.setTextColor(TFT_WHITE);
   tft.setTextSize(1);
   tft.drawString("Version",0, 10);
-  tft.drawString("1.0-f",0, 20);
+  tft.drawString("1.1",0, 20);
   tft.drawString("Battery",0, 30);
   tft.drawString("NULL",0, 40);
   tft.drawString("User ID",0, 50);
@@ -435,6 +448,133 @@ void shortpresshandler(){
   }
 }
 
+//only for mqtt profile fetching
+void reconnect2(){
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("WB1",mqttuser,mqttpass)) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("clients/wb/wb1config");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void mqttgetprofile(char* topic, byte* message, unsigned int length) {
+  //double interlock, avoid multiple runs
+  if(!configured){
+    String messageTemp;
+    String time[6];
+    int fragment = 1;
+    int peri = 0;
+    int zoneni = 0;
+    int zoneri = 0;
+    int timei = 0;
+
+
+    //sample data: username > time(year,month,day,hh,mm,ss) > permitted zone rating(split using ,)> zone names(split using ,) array > corresponding zone rating(split using ,) array
+    if (String(topic) == "clients/wb/wb1config") {
+      Serial.println("Profile received ");
+      for (int i = 0; i < length; i++) {
+        messageTemp += (char)message[i];
+        if(message[i] == '>'){
+          fragment++;
+          continue;
+        }
+        if(fragment == 1){
+          user += (char)message[i];
+        }
+        if(fragment == 2){
+          if(message[i] == ','){
+            timei ++;
+            continue;
+          }
+          time[timei] += (char)message[i];
+          //(year,month,day,hour,min,sec)
+          rtc.setDateTime(time[0].toInt(), time[1].toInt(), time[2].toInt(), time[3].toInt(), time[4].toInt(), time[5].toInt());
+        }
+        if(fragment == 3){
+          if(message[i] == ','){
+            peri ++;
+            continue;
+          }
+          permitted[peri] = (char)message[i];
+        }
+        if(fragment == 4){
+          if(message[i] == ','){
+            zoneni ++;
+            continue;
+          }
+          zoneid[zoneni] += (char)message[i];
+        }
+        if(fragment == 5){
+          if(message[i] == ','){
+            zoneri ++;
+            continue;
+          }
+          allowedzonerating[zoneri] = (char)message[i];
+        }
+      }
+    }
+    Serial.println("Name: " + user);
+    Serial.print("Time set to: ");
+    Serial.println(rtc.formatDateTime(PCF_TIMEFORMAT_YYYY_MM_DD_H_M_S));
+    Serial.println("permitted zones");
+    for(int l = 0; l < 50; l++)
+    {
+    Serial.print(permitted[l]);
+    Serial.print(',');
+    }
+    Serial.println("");
+    Serial.println("all zone zoneids");
+    for(int l = 0; l < 50; l++)
+    {
+    Serial.print(zoneid[l]);
+    Serial.print(',');
+    }
+    Serial.println("");
+    Serial.println("all zone zone ratings");
+    for(int l = 0; l < 50; l++)
+    {
+    Serial.print(allowedzonerating[l]);
+    Serial.print(',');
+    }
+    Serial.println("");
+    configured = true;
+  }
+}
+
+void getprofile(){
+  //configure callback function for profile reception
+  Serial.println("Awaiting Profile");
+  client.setCallback(mqttgetprofile);
+  while(!configured){
+    if (!client.connected()) {
+      reconnect2();
+    }
+    //if there is message this will process it
+    client.loop();
+    //send request(send uuid)
+    client.publish("clients/wb/wb1profilereq", hwuuid);
+    delay(1500);
+  }
+  //reset mqtt client
+  client.unsubscribe("clients/wb/wb1config");
+  //client.subscribe("clients/wb/wb1");
+  delay(100);
+  client.loop();
+  client.disconnect();
+  client.setCallback(callback);
+}
+
+
 void setup() {
   Serial.begin(115200);
   pinMode(LCD_BL,OUTPUT);
@@ -449,7 +589,7 @@ void setup() {
   setup_wifi();
   //MQTT
   client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  //client.setCallback(callback);
   //rtc
   Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
   Wire.setClock(400000); //I2C frequency
@@ -464,10 +604,19 @@ void setup() {
   startble();
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE);
-  tft.drawString("Wristband v1.0",  0, tft.height() / 2 - 20);
+  tft.drawString("Wristband v1.1",  0, tft.height() / 2 - 20);
   tft.drawString("BLE OK",  0, tft.height() / 2  + 20);
   tft.drawString("BOOTED",  0, tft.height() / 2  + 60);
   delay(3000); //stablize
+
+
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
+  tft.drawString("Await",  0, tft.height() / 2 - 20);
+  tft.drawString("User",  0, tft.height() / 2  + 20);
+  tft.drawString("Profile",  0, tft.height() / 2  + 60);
+
+  getprofile();
 
   homescreen();
 
