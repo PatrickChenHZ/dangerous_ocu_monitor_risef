e5984f290ce1ae73e6ce67ddae34ae40c3e7e80b
PatrickChina <33338982+PatrickChina@users.noreply.github.com>
2022-01-29 15:01:00 -0500

    Upload MPU6050 Testing Code
    The code used for trail data obtaining for CSV collecting

---
+++ Componets_Prototype/Fall_Detection/MPU6050_ACC_Trail/MPU6050_ACC_Trail.ino
@@ -0,0 +1,59 @@
+// Basic demo for accelerometer readings from Adafruit MPU6050
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  //Serial.println("MPU6050 Found!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  delay(100);
+}
+
+void loop() {
+
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  //Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print("\t");
+  Serial.print(a.acceleration.y);
+  Serial.print("\t");
+  Serial.println(a.acceleration.z);
+  //Serial.println(" m/s^2");
+
+  /*s
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+  */
+  delay(30);
+}
